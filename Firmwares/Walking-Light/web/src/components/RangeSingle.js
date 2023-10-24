29c42e44ae17b9d02664ca38b15b50d47a81ec75
Oleg Perevyshin <oleg.perevyshin@soi-tech.com>
2023-10-24 07:32:52 +0300

    Web -> Project

---
+++ Firmwares/Walking-Light/web/src/components/RangeSingle.js
@@ -0,0 +1,26 @@
+import { h, Component } from 'preact';
+
+class RangeSingle extends Component {
+  render({ orientation, value, className, ...rest }, { current_value }) {
+    if (isNaN(current_value))
+      if (isNaN(value)) current_value = 0;
+      else current_value = value;
+    let baseClassName = 'range input ';
+    orientation = orientation || 'horizontal';
+    baseClassName += orientation;
+    className = className ? baseClassName + ' ' + className : baseClassName;
+    return (
+      <label className={className}>
+        <input
+          onInput={this.linkState('current_value')}
+          type="range"
+          value={current_value}
+          {...rest}
+        />
+        <span>{current_value}</span>
+      </label>
+    );
+  }
+}
+
+export default RangeSingle;
